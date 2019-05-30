20b832c4798c4275e50905901762a0e9c2df58ea
lilianwaweru <lilowesh.lw@gmail.com>
2019-05-30 15:35:57 +0300

    added a url for index.html

--- neighbour/urls.py
+++ neighbour/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-   url(r'^$',views.welcome,name = 'welcome')
-
+   url('^$',views.index,name = 'index'),
+   url(r'posts/$',views.welcome,name = 'welcome'),
 ]   
