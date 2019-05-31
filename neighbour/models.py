eccc804645626c4e757236394e8e4173006addea
lilianwaweru <lilowesh.lw@gmail.com>
2019-05-31 11:18:48 +0300

    added a view for profile

--- neighbour/models.py
+++ neighbour/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
+
 # Create your models here.
+class Profile(models.Model):
+    profile_picture = models.ImageField(upload_to = 'images/',blank=True)
+    bio = models.CharField(max_length = 70)
+    contact = models.CharField(max_length=12)
+    # project = models.ForeignKey('Project',on_delete=models.CASCADE,null=True)
+    # user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def update_bio(self,bio):
+        self.bio = bio
+        self.save()
+
+
+    def __str__(self):
+        return f'{self.bio}'      
--- neighbour/views.py
+++ neighbour/views.py
@@ -8,3 +8,21 @@ def welcome(request):
 def index(request):
     title = "Index Page"
     return render (request, 'index.html', {"title":title})
+
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    logged_user = request.user
+    if request.method == 'POST':
+        form = EditProfile(request.POST,request.FILES)
+        if form.is_valid():
+            edit = form.save(commit=False)
+            edit.user = logged_user
+            edit.save()
+        return redirect('welcome')
+
+    else:
+
+        form = EditProfile()
+
+    return render(request,'profile.html',{'form':form})
